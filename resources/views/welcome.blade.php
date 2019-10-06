627898618e909175a12b6fa8dd56143ed504812a
kazukidomae <donmainakanaideyo@gmail.com>
2019-10-06 21:51:50 +0900

    トップページの一部を修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    HashiGO
+                    HashiGO!
                 </div>
             </div>
         </div>
