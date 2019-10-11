a283d95c1ab16960a84d5fb61682275070554573
ths71090 <79cibi@gmail.com>
2019-10-11 16:47:37 +0900

    ピン立て

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
+import { callbackify } from 'util'
 Vue.use(VueGoogleMaps, {
 load: {
     key: '',
-    libraries: 'places'
+    libraries: 'places',
     }
 })
