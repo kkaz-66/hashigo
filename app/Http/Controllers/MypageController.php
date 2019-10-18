5af3fdcc0877305b302b581882b261c105ff5c3d
vagrant <vagrant@homestead>
2019-10-18 06:28:28 +0000

    viewからコントローラアクセス、返り値不明。。

--- app/Http/Controllers/MypageController.php
+++ app/Http/Controllers/MypageController.php
@@ -10,45 +10,28 @@ use App\User;
 
 class MypageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $user = Auth::user();
         return view('mypage',compact('user'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function seach_shop($id)
     {
-        //
+        $hpg_key = config('apikey.hpg-key');
+        $url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?key=".$hpg_key."&id=".$id."&format=json";
+
+        return $this->create_list($url);
     }
 
-    public function insert()
+    public function create_list($url)
     {
-        //
+        $json = array();
+        $json = file_get_contents($url);
+        $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+        $arr = json_decode($json,true);
+        return $arr['results']['shop'];
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
 
     public function hashigo_history()
     {
@@ -57,38 +40,4 @@ class MypageController extends Controller
         return view('mypage',compact('user','hashigos'));
     }
 
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -1,37 +1,18 @@
 @extends('layouts.app')
 @section('content')
-@inject('hahigo_history','App\Http\Controllers\MypageController')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>mypage</title>
-</head>
-<body>
-   
-    @foreach ($hashigos as $hashigo)
-        @if($user->id == $hashigo->id)
-            {{$hashigo->member_id}}
-            {{$hashigo->id}}
-            {{$hashigo->first_store_id}}
-            {{$hashigo->second_store_id}}
-            {{$hashigo->third_store_id}}
-            {{$hashigo->created_at}}
-            <br>
-        @endif
-    @endforeach
-
-    <div id="app">
-        <header-component></header-component>
-        <mypage-component></mypage-component>
-        <footer-component></footer-component>
-    </div>
-
-    <script src=" {{ mix('js/app.js') }} "></script>
-</body>
-</html>
+@inject('hashigo_arr','App\Http\Controllers\MypageController')
+@foreach ($hashigos as $hashigo)
+    @if($user->id == $hashigo->member_id)
+        {{$hashigo->member_id}}
+        {{$hashigo->id}}
+        {{$hashigo->first_store_id}}
+        {{$hashigo->second_store_id}}
+        {{$hashigo->third_store_id}}
+        {{$hashigo->created_at}}<br>
+        hashigo shop: {{ ($hashigo_arr["results"]["shop"]) -> seach_shop($hashigo->first_store_id) }}
+        <br>
+    @endif
+@endforeach
+<mypage-component></mypage-component>
+<footer-component></footer-component>
 @endsection
