6c2060b8702a675ecfca9585a7296bdc591bdb05
vagrant <vagrant@homestead>
2019-10-18 09:44:29 +0000

    TOPレイアウト作成

--- public/js/app.js
+++ public/js/app.js
@@ -1769,6 +1769,9 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
 //
 //
 //
+//
+//
+//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
@@ -1788,10 +1791,10 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       geocode: {},
       address: '',
       center: {
-        lat: 37.71,
-        lng: 139.72
+        lat: 35.6581,
+        lng: 139.7017
       },
-      zoom: 14,
+      zoom: 18,
       marker_items: [],
       icon: {
         url: "",
@@ -1821,25 +1824,18 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
         }, function (results, status) {
           resolve(results[0].geometry.location);
         });
+
+        _this.isActive = true;
       });
     },
     //現在地のピン立て
-    getCurrentPositionSuccess: function getCurrentPositionSuccess(position) {
-      this.marker_items = [];
-      var lat = position.coords.latitude;
-      var lng = position.coords.longitude;
-      this.$refs.map.panTo({
-        lat: lat,
-        lng: lng
-      });
-      this.marker_items.push({
-        position: {
-          lat: lat,
-          lng: lng
-        },
-        title: 'marker_5'
-      });
-    },
+    // getCurrentPositionSuccess (position) {
+    //     this.marker_items=[];
+    //     let lat = position.coords.latitude
+    //     let lng = position.coords.longitude
+    //     this.$refs.map.panTo({lat: lat, lng: lng})
+    //     this.marker_items.push({position: {lat: lat, lng: lng}, title: 'marker_5'})
+    // },
     //ピン立て 中央
     setcentermarker: function setcentermarker(lat, lng) {
       this.marker_items = [];
@@ -1852,7 +1848,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
           lat: lat,
           lng: lng
         },
-        title: '現在地',
+        title: '中心地',
         icon: {
           url: 'http://pictogram2.com/p/p0957/3.png',
           scaledSize: new google.maps.Size(50, 55),
@@ -1981,7 +1977,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
       this.lat = this.marker_items[id].position.lat;
       this.lng = this.marker_items[id].position.lng;
 
-      if (this.marker_items[id].title == '現在地') {
+      if (this.marker_items[id].title == '中心地') {
         this.isActive = true;
       } else {
         this.isActive = false;
@@ -9025,7 +9021,7 @@ exports = module.exports = __webpack_require__(/*! ../../../node_modules/css-loa
 
 
 // module
-exports.push([module.i, "\n#map[data-v-332fccf4] {\n    width: 100%;\n    height: 855px;\n} \n\n", ""]);
+exports.push([module.i, "\n.row[data-v-332fccf4] {\n    padding: 10px;\n}\n#search[data-v-332fccf4] {\n    display: inline-block;\n    padding: 0.5em 1em;\n    text-decoration: none;\n    background: #668ad8;/*ボタン色*/\n    color: #FFF;\n    border-bottom: solid 4px #627295;\n    border-radius: 3px;\n}\n#search[data-v-332fccf4]:active {\n    /*ボタンを押したとき*/\n    transform: translateY(4px);/*下に動く*/\n    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.2);/*影を小さく*/\n    border-bottom: none;\n}\n#textbox[data-v-332fccf4] {\n    width: 360px;\n    padding: 5px 8px;\n    border-radius: 6px;\n    border-top: 1px solid #aaa;\n    border-left: 1px solid #aaa;\n    border-right: 2px solid #aaa;\n    border-bottom: 2px solid #aaa;\n    background-image: none;\n    background-color: rgb(255, 254, 254);\n    font-size: 16px;\n}\n#area[data-v-332fccf4] {\n    display: inline-block;\n    padding: 0.5em 1em;\n    text-decoration: none;\n    background: #668ad8;/*ボタン色*/\n    color: #FFF;\n    border-bottom: solid 4px #627295;\n    border-radius: 3px;\n}\n#area[data-v-332fccf4]:active {\n    /*ボタンを押したとき*/\n    transform: translateY(4px);/*下に動く*/\n    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.2);/*影を小さく*/\n    border-bottom: none;\n}\n.col-md-9[data-v-332fccf4] {\n    margin-left: 20px;\n    margin-right: -20px;\n    position: relative;\n    border-top: solid 2px black;\n    border-bottom: solid 2px black;\n}\n.col-md-9[data-v-332fccf4]:before, .col-md-9[data-v-332fccf4]:after {\n    content: '';\n    position: absolute;\n    top: -10px;\n    width: 2px;\n    height: calc(100% + 20px);\n    background-color: black;\n}\n.col-md-9[data-v-332fccf4]:before {\n    left: 10px;\n}\n.col-md-9[data-v-332fccf4]:after {\n    right: 10px;\n}\n#map[data-v-332fccf4] {\n    width: 100%;\n    height: 750px;\n}\n.shopname[data-v-332fccf4] {\n    background: linear-gradient(transparent 70%, #ff99ff 70%);\n}\n#shop[data-v-332fccf4] {\n    text-align: center;\n    background-color: rgb(255, 247, 170);\t/* 背景色 */\n    border: 1px solid rgb(255, 255, 255); /* 線の太さ・種類・色 */\n    box-shadow:1px 1px 6px 0px #ccc;\n    -moz-box-shadow:1px 1px 6px 0px #ccc;\n    -webkit-box-shadow:1px 1px 6px 0px #ccc;\n    -o-box-shadow:1px 1px 6px 0px #ccc;\n    margin: 20px 5px; /* 外側の余白 上下・左右 */\n    padding: 10px; /* 内側の余白 上・右・下・左 */\n    position: relative;\n    z-index: 0;\n}\n#shop[data-v-332fccf4]:before {\n    border: 1px solid #fff; /* 白い実線 */\n    border-radius: 5px;\n    content: '';\n    display: block;\n    margin: 4px;\n    position: absolute;\n    top: 0px;\n    bottom: 0px;\n    left: 0px;\n    right: 0px;\n    z-index: -1;\n}\n#shop[data-v-332fccf4]:after {\n    background-color: rgba(250, 178, 232, 0.979);  /* テープ背景色 */\n    background-image: radial-gradient(#fff 20%, transparent 0), radial-gradient(#fff 20%, transparent 0); /* 水玉の色 */\n    background-position: 0 0, 8px 8px;  /* 水玉の距離 */\n    background-size: 15px 15px; /* 水玉の大きさ */\n    border-left: 2px dotted rgba(0,0,0,0.1);\n    border-right: 2px dotted rgba(0,0,0,0.1);\n    box-shadow: 0 0 5px rgba(0,0,0,0.2);\n    content: '\\5019\\88DC';\n    display: block;\n    margin-left: 160px;\n    padding: 5px 20px;  \n    text-align: center;\n    position: absolute;\n    top: -10px;\n    left: 20px;\n    transform: rotate(-3deg);\n    -moz-transform: rotate(-3deg);\n    -webkit-transform: rotate(-3deg);\n    -o-transform: rotate(-3deg);\n}\n#hot[data-v-332fccf4] {\n    padding-left: 35px;\n    padding-top: 10px;\n}\n", ""]);
 
 // exports
 
@@ -48407,13 +48403,16 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "body" }, [
     _c("div", { staticClass: "app" }, [
-      _vm._m(0),
-      _vm._v(" "),
       _c("div", { staticClass: "row" }, [
         _c("div", { staticClass: "col-md-12" }, [
-          _c("button", { on: { click: _vm.currentsearch } }, [
-            _vm._v("現在地へ移動")
-          ]),
+          _c(
+            "button",
+            {
+              attrs: { id: "search", type: "submit" },
+              on: { click: _vm.currentsearch }
+            },
+            [_vm._v("現在地へ移動")]
+          ),
           _vm._v(" "),
           _c("input", {
             directives: [
@@ -48424,7 +48423,7 @@ var render = function() {
                 expression: "address"
               }
             ],
-            attrs: { type: "text" },
+            attrs: { id: "textbox", type: "text", placeholder: "エリア検索" },
             domProps: { value: _vm.address },
             on: {
               input: function($event) {
@@ -48438,7 +48437,10 @@ var render = function() {
           _vm._v(" "),
           _c(
             "button",
-            { attrs: { type: "button" }, on: { click: _vm.keywordSearch } },
+            {
+              attrs: { id: "area", type: "submit" },
+              on: { click: _vm.keywordSearch }
+            },
             [_vm._v("検索")]
           )
         ])
@@ -48482,48 +48484,63 @@ var render = function() {
           )
         ]),
         _vm._v(" "),
-        _c("div", { staticClass: "shop" }, [
-          _vm.isActive
-            ? _c("div")
-            : _c(
-                "div",
-                {
-                  staticClass: "col-md-3",
-                  staticStyle: { "white-space": "nowrap" }
-                },
-                [
-                  _c("img", { attrs: { src: _vm.photo } }),
-                  _c("br"),
-                  _vm._v("\n                    " + _vm._s(_vm.name)),
-                  _c("br"),
-                  _vm._v(" "),
-                  _c("a", { attrs: { href: _vm.url } }, [_vm._v("店情報")]),
-                  _c("br"),
-                  _vm._v("\n                    " + _vm._s(_vm.id)),
-                  _c("br"),
-                  _vm._v("\n                    " + _vm._s(_vm.lat)),
-                  _c("br"),
-                  _vm._v("\n                    " + _vm._s(_vm.lng)),
-                  _c("br"),
-                  _vm._v(" "),
-                  _c(
-                    "a",
-                    {
-                      attrs: {
-                        href:
-                          _vm.test +
-                          _vm.id +
-                          _vm.test2 +
-                          _vm.lat +
-                          _vm.test3 +
-                          _vm.lng
-                      }
-                    },
-                    [_vm._v("詳細")]
-                  )
-                ]
-              )
-        ])
+        _c("div", { staticClass: "col-md-3" }, [
+          _c("div", [
+            _vm.isActive
+              ? _c("div")
+              : _c(
+                  "div",
+                  {
+                    staticStyle: { "white-space": "nowrap" },
+                    attrs: { id: "shop" }
+                  },
+                  [
+                    _c("img", { attrs: { src: _vm.photo } }),
+                    _c("br"),
+                    _vm._v(" "),
+                    _c("h3", [
+                      _c("span", { staticClass: "shopname" }, [
+                        _vm._v(_vm._s(_vm.name))
+                      ]),
+                      _c("br")
+                    ]),
+                    _c("br"),
+                    _vm._v(" "),
+                    _c("a", { attrs: { href: _vm.url } }, [
+                      _vm._v("ホットぺッパー")
+                    ]),
+                    _c("br"),
+                    _vm._v(" "),
+                    _c("h5", [_vm._v(_vm._s(_vm.id))]),
+                    _c("br"),
+                    _vm._v(" "),
+                    _c("h5", [_vm._v(_vm._s(_vm.lat))]),
+                    _c("br"),
+                    _vm._v(" "),
+                    _c("h5", [_vm._v(_vm._s(_vm.lng))]),
+                    _c("br"),
+                    _vm._v(" "),
+                    _c(
+                      "a",
+                      {
+                        attrs: {
+                          href:
+                            _vm.test +
+                            _vm.id +
+                            _vm.test2 +
+                            _vm.lat +
+                            _vm.test3 +
+                            _vm.lng
+                        }
+                      },
+                      [_vm._v("詳細")]
+                    )
+                  ]
+                )
+          ])
+        ]),
+        _vm._v(" "),
+        _vm._m(0)
       ])
     ])
   ])
@@ -48533,9 +48550,22 @@ var staticRenderFns = [
     var _vm = this
     var _h = _vm.$createElement
     var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "row" }, [
-      _c("div", { staticClass: "col-md-12" }, [_c("p", [_vm._v("Hashigo")])])
-    ])
+    return _c(
+      "a",
+      { attrs: { id: "hot", href: "http://webservice.recruit.co.jp/" } },
+      [
+        _c("img", {
+          attrs: {
+            src: "http://webservice.recruit.co.jp/banner/hotpepper-s.gif",
+            alt: "ホットペッパー Webサービス",
+            width: "135",
+            height: "17",
+            border: "0",
+            title: "ホットペッパー Webサービス"
+          }
+        })
+      ]
+    )
   }
 ]
 render._withStripped = true
