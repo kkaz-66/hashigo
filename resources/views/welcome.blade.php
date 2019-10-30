9b91fef0c2900e7a7fa2652fc761872098755238
vagrant <vagrant@homestead>
2019-10-30 02:32:32 +0000

    welcome iphonX comp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,49 +11,6 @@
 
         <!-- Styles -->
         <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
-        <style>
-            html, body {
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .welcome_wrap {
-                display: flex;
-                position: relative;
-                width: 100%;
-                height: 100%;
-                background-color: rgba(255, 255, 255, 0.3);
-            }
-
-            .welcome_link {
-                display: block;
-                position: absolute;
-                width: 100%;
-                height: 100%;
-                z-index: 9;
-            }
-
-            .content {
-                display: flex;
-                position: absolute;
-                text-align: center;
-                width: 100%;
-                height: 100%;
-            }
-
-            .welcome_hashigo {
-                display: flex;
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                font-size: 8rem;
-                transform: translateY(-50%) translateX(-50%);
-                -webkit- transform: translateY(-50%) translateX(-50%);
-            }
-        </style>
     </head>
     <body>
         <div class="welcome_wrap">
